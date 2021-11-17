5d8f8ec107240942e6469863b5df8b744fff6c10
mihirkumar1212 <pmihir2609@gmail.com>
2021-11-16 21:51:51 -0500

    Database created and Geocoding working

--- app/src/main/java/com/example/locationfinder/Add_Location.java
+++ app/src/main/java/com/example/locationfinder/Add_Location.java
@@ -1,7 +1,7 @@
 package com.example.locationfinder;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -10,58 +10,81 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 public class Add_Location extends AppCompatActivity {
-    TextView input1, input2, address_new;
-    Button generateAddress;
-
-
-    //Google API for location services.
-    FusedLocationProviderClient fusedLocationProviderClient;
-
+    TextView input1, input2, address_new, randomNumber;
+    Button generateAddress, random_button, save_button;
+    MyDatabase database;
+    double longitude, latitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
 
-
         input1 = findViewById(R.id.input_1);
         input2 = findViewById(R.id.input_2);
         address_new = findViewById(R.id.Address_new);
         generateAddress = findViewById(R.id.GenrateAddress);
+        random_button = findViewById(R.id.Random_button);
+        randomNumber = findViewById(R.id.random_number);
+        save_button = findViewById(R.id.Save);
 
 
         generateAddress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double longitude = Double.parseDouble(input1.getText().toString());
-                Double latitude = Double.parseDouble(input2.getText().toString());
-                Geocoder geocoder = new Geocoder(Add_Location.this, Locale.getDefault());
 
+                // Gets the address form the user
+                Geocoder geo = new Geocoder(Add_Location.this, Locale.getDefault());
+
+                longitude = Double.parseDouble(input2.getText().toString());
+                latitude = Double.parseDouble(input1.getText().toString());
                 try {
-                    List<Address>listAddress = geocoder.getFromLocation(44,-80,1);
+                    List<Address> listAddress = geo.getFromLocation(latitude,longitude,1);
                     if (listAddress.size() > 0){
-                        //address_new.setText(listAddress.get(0).getCountryName());
-                        Toast.makeText(Add_Location.this,listAddress.get(0).getCountryName(),Toast.LENGTH_LONG ).show();
+                        address_new.setText(listAddress.get(0).getAddressLine(0));
+                        // Toast.makeText(Add_Location.this, listAddress.get(0).getCountryName(),Toast.LENGTH_SHORT).show();
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+        });
 
+
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String address = address_new.getText().toString();
+                // calling a function to store the data in the database.
+                database = new MyDatabase(Add_Location.this);
+                database.addLocation(address,String.valueOf(latitude),String.valueOf(longitude));
 
             }
         });
 
 
-
-
-
-
+        random_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double result = Double.parseDouble(randomNumber(-90.0, 90.0));
+                double result2 = Double.parseDouble(randomNumber(-180.0, 80.0));
+                input1.setText(String.valueOf(result));
+                input2.setText(String.valueOf(result2));
+                //randomnumber.setText(String.format("%.2f",result));
+            }
+        });
     }
+
+    public String randomNumber (Double min, Double max){
+        double num;
+        num = (Math.random() * (max - min)) + min;
+
+        return String.format("%.1f", num);
+    }
+
 }
--- app/src/main/java/com/example/locationfinder/MyDatabase.java
+++ app/src/main/java/com/example/locationfinder/MyDatabase.java
@@ -1,8 +1,11 @@
 package com.example.locationfinder;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class MyDatabase  extends SQLiteOpenHelper {
 
@@ -26,8 +29,8 @@ public class MyDatabase  extends SQLiteOpenHelper {
         String query = "CREATE TABLE " + Table_Name +
                 " (" + Column_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 Column_ADDRESS + " TEXT," +
-                Column_Latitude + " DOUBLE," +
-                Column_Longitude + " DOUBLE);";
+                Column_Latitude + " TEXT," +
+                Column_Longitude + " TEXT);";
         db.execSQL(query);
 
     }
@@ -37,4 +40,42 @@ public class MyDatabase  extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + Table_Name);
         onCreate(db);
     }
+
+    void addLocation(String address, String latitude, String longitude) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Column_ADDRESS, address);
+        contentValues.put(Column_Latitude, latitude);
+        contentValues.put(Column_Longitude, longitude);
+
+        long check_result = db.insert(Table_Name, null, contentValues);
+        if (check_result == -1) {
+            Toast.makeText(context, "Failed to add note", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Note Added Successfully!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    Cursor getData() {
+        String query = "SELECT * FROM " + Table_Name;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if (db != null) {
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+    Cursor getSearchData(String target) {
+
+        String query = "SELECT * FROM " + Table_Name + " WHERE " + Column_ADDRESS +" Like " + "'" +target + "%'";
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if (db != null) {
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
 }
