34a1748b19fd7ca7b4c04d145a281bcfc8193239
mihirkumar1212 <pmihir2609@gmail.com>
2021-11-17 23:55:29 -0500

    Final Updates

--- app/src/main/java/com/example/locationfinder/Add_Location.java
+++ app/src/main/java/com/example/locationfinder/Add_Location.java
@@ -30,8 +30,8 @@ public class Add_Location extends AppCompatActivity {
         input2 = findViewById(R.id.input_2);
         address_new = findViewById(R.id.Address_new);
         generateAddress = findViewById(R.id.GenrateAddress);
-        random_button = findViewById(R.id.Random_button);
-        randomNumber = findViewById(R.id.random_number);
+        //random_button = findViewById(R.id.Random_button);
+        //randomNumber = findViewById(R.id.random_number);
         save_button = findViewById(R.id.Save);
 
 
@@ -50,40 +50,58 @@ public class Add_Location extends AppCompatActivity {
                 // calling a function to store the data in the database.
                 database = new MyDatabase(Add_Location.this);
                 database.addLocation(address,String.valueOf(latitude),String.valueOf(longitude));
-
                 Intent intent = new Intent(Add_Location.this, MainActivity.class);
                 startActivity(intent);
+
             }
         });
 
 
-        random_button.setOnClickListener(new View.OnClickListener() {
+        /*random_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double result = Double.parseDouble(randomNumber(-90.0, 90.0));
-                double result2 = Double.parseDouble(randomNumber(-180.0, 80.0));
-                input1.setText(String.valueOf(result));
-                input2.setText(String.valueOf(result2));
+
+                //randomNum();
             }
-        });
+        });*/
+    }
+    public void randomNum(){
+        double result = Double.parseDouble(randomNumber(-90.0, 90.0));
+        double result2 = Double.parseDouble(randomNumber(-180.0, 80.0));
+        input1.setText(String.valueOf(result));
+        input2.setText(String.valueOf(result2));
     }
 
-
     public  void getAddress(){
+        //randomNum();
         Geocoder geo = new Geocoder(Add_Location.this, Locale.getDefault());
 
         longitude = Double.parseDouble(input2.getText().toString());
         latitude = Double.parseDouble(input1.getText().toString());
-        try {
-            List<Address> listAddress = geo.getFromLocation(latitude,longitude,1);
-            if (listAddress.size() > 0){
-                address_new.setText(listAddress.get(0).getAddressLine(0));
-                //Toast.makeText(Add_Location.this, listAddress.get(0).getCountryName(),Toast.LENGTH_SHORT).show();
+        if(latitude >= -90.0 && latitude  <= 90.0 && longitude >= -180.0 && longitude <= 80.0) {
+            try {
+                List<Address> listAddress = geo.getFromLocation(latitude, longitude, 1);
+                if(listAddress.size() == 0){
+                    Toast.makeText(Add_Location.this, "No address found try different coordinates",Toast.LENGTH_SHORT).show();
+                }
+                if (listAddress.size() > 0) {
+                    if (listAddress.get(0).getAddressLine(0) != null) {
+                        address_new.setText(listAddress.get(0).getAddressLine(0));
+                        Toast.makeText(Add_Location.this, "Found address",Toast.LENGTH_SHORT).show();
+                        if(listAddress.get(0).getPostalCode() != null){
+                            String temp = listAddress.get(0).getAddressLine(0) + listAddress.get(0).getPostalCode();
+                            address_new.setText(temp);
+                            Toast.makeText(Add_Location.this, "Found address and Postal code",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    //Toast.makeText(Add_Location.this, listAddress.get(0).getCountryName(),Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }else{
+            Toast.makeText(Add_Location.this, "Invalid Longitude and Latitude!",Toast.LENGTH_SHORT).show();
         }
-
     }
     public String randomNumber (Double min, Double max){
         double num;
